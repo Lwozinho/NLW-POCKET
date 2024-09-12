b99561f1ba28b2e03ea94d6b24bdc57bb9570085
Leonardo Nascimento <leonardopintonascimento@gmail.com>
2024-09-12 18:00:47 -0300

    metas realizadas

--- index.js
+++ index.js
@@ -48,6 +48,22 @@ const listarMetas = async () => {
 
 }
 
+const metasRealizadas = async ()=> {
+    const realizadas = metas.filter((meta) => {
+      return meta.checked
+    })
+
+    if(realizadas.length == 0) {
+      console.log("Não existem metas realizadas")
+      return
+    }
+
+    await select({
+      message: "Metas Realizadas",
+      choices: [...realizadas]
+    })
+}
+
 
 
 const start = async ()=> {
@@ -66,6 +82,14 @@ const start = async ()=> {
           value: "listar"
         },
         {
+          name: "Metas realizadas",
+          value: "realizadas"
+        },
+        {
+          name: "Metas abertas",
+          value: "abertas"
+        },
+        {
           name: "Sair",
           value: "sair"
         }
@@ -81,6 +105,15 @@ const start = async ()=> {
         case "listar":
           await listarMetas()
           break
+
+        case "realizadas":
+          await metasRealizadas()
+          break
+
+        case "abertas":
+          await metasAbertas()
+          break
+        
         case "sair":
           console.log("Até a próxima!")
           return
