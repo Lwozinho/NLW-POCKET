c218670714fed2162f323867ada7b67a2cf96f2e
Leonardo Nascimento <leonardopintonascimento@gmail.com>
2024-09-12 19:51:43 -0300

    persistir dados

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
 const { select, input, checkbox } = require('@inquirer/prompts')
+const fs = require("fs").promises
 
 let mensagem = "Bem vindo ao App de metas";
 
-let meta = {
-    value: 'Tomar 3L de água por dia',
-    checked: false,
+let metas
+
+const carregarMetas = async () => {
+  try{
+    const dados = await fs.readFile("metas.json", "utf-8")
+    metas = JSON.parse(dados)
+  }
+  catch(erro){
+      metas = []
+  }
 }
 
-let metas = [meta]
+const salvarMetas = async () => {
+  await fs.writeFile("metas.json", JSON.stringify(metas, null, 2))
+}
+
 
 const cadastrarMeta = async () => {
     const meta = await input({ message: "Digite a meta" })
@@ -25,6 +36,12 @@ const cadastrarMeta = async () => {
 }
 
 const listarMetas = async () => {
+      if(metas.length == 0) {
+        mensagem = "Não existem metas!"
+        return
+      }
+
+
   const respostas = await checkbox({
     message: "Use as setas para mudar de meta, o espaço para marcar ou desmarcar e o Enter para finalizar essa etapa",
     choices: [...metas],
@@ -54,6 +71,10 @@ mensagem = "Meta(s) marcada(s) concluída(s)!"
 }
 
 const metasRealizadas = async ()=> {
+  if(metas.length == 0) {
+    mensagem = "Não existem metas!"
+    return
+  }
     const realizadas = metas.filter((meta) => {
       return meta.checked
     })
@@ -71,6 +92,11 @@ const metasRealizadas = async ()=> {
 
 
 const metasAbertas = async () => {
+  if(metas.length == 0) {
+    mensagem = "Não existem metas!"
+    return
+  }
+
   const abertas = metas.filter((meta) => {
     return meta.checked != true
   })
@@ -87,6 +113,11 @@ const metasAbertas = async () => {
 }
 
 const deletarMetas = async () => {
+  if(metas.length == 0) {
+    mensagem = "Não existem metas!"
+    return
+  }
+  
   const metasDesmarcadas = metas.map((meta) => {
     return {value: meta.value, checked:false}
   })
@@ -123,9 +154,12 @@ const mostrarMensagem = () => {
 
 
 const start = async ()=> {
+  await carregarMetas()
+  
   
   while(true) {
     mostrarMensagem()
+    await salvarMetas()
 
      const opcao = await select({
       message:"Menu >",
@@ -161,10 +195,12 @@ const start = async ()=> {
       switch(opcao) {
         case "cadastrar":
          await cadastrarMeta()
+        
 
           break
         case "listar":
           await listarMetas()
+        
           break
 
         case "realizadas":
@@ -177,6 +213,7 @@ const start = async ()=> {
 
         case "deletar":
           await deletarMetas()
+         
           break 
 
         case "sair":
@@ -187,3 +224,6 @@ const start = async ()=> {
   }
 
 start()
+
+
+
